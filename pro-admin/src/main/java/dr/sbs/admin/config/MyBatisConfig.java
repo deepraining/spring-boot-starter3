38930f69882e753fd08ef03a127fb5dd6e4475fb
deepraining <jiangjinbelief@163.com>
2026-08-10 10:40:16 +0800

    add mysql page extension

--- pro-admin/src/main/java/dr/sbs/admin/config/MyBatisConfig.java
+++ pro-admin/src/main/java/dr/sbs/admin/config/MyBatisConfig.java
@@ -1,10 +1,23 @@
 package dr.sbs.admin.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
 @MapperScan({"dr.sbs.mp.mapper", "dr.sbs.admin.dao"})
-public class MyBatisConfig {}
+public class MyBatisConfig {
+  @Bean
+  public MybatisPlusInterceptor mybatisPlusInterceptor() {
+    MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+
+    interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+
+    return interceptor;
+  }
+}
